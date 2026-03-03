ca43c09cf231365fe473e9efc67c15b25c6d74f3
GintareeJ <gjonaityte1@gmail.com>
2026-03-03 20:41:25 +0200

    Rename header guard and add Skaiciavimai function

--- v0.3/pasirinkimai.h
+++ v0.3/pasirinkimai.h
@@ -1,13 +1,14 @@
-#ifndef APDOROJIMAS_H
-#define APDOROJIMAS_H
+#ifndef PASIRINKIMAI_H
+#define PASIRINKIMAI_H
 
 #include <vector>
 #include "studentas.h"
 #include "rusiavimas.h"
 
+void Skaiciavimai(std::vector<studentas>& studentai);
 void PirmasP(std::vector<studentas>& studentai);
 void AntrasP(std::vector<studentas>& studentai);
 void TreciasP(std::vector<studentas>& studentai, const std::vector<std::string>& pavardes,  const std::vector<std::string>& vardai);
-void KetvirtasP(std::vector<studentas>& studentai, const std::string& CVfd, const std::string& CVfr, double& suma2, double& suma3, double& suma4, double& suma5, double& suma6, int& spausd);
+void KetvirtasP(std::vector<studentas>& studentai, const std::string& CVfd, const std::string& CVfr, double& suma2, double& suma3, double& suma4, double& suma5, double& suma6, int& spausd, bool& skaitytaIsFailo);
 
 #endif
