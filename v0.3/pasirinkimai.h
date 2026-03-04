a4b89fc4bcb281a99dc03af3f7bb0565b85183e9
GintareeJ <gjonaityte1@gmail.com>
2026-03-04 17:34:56 +0200

    Add new functions and parameters to pasirinkimai.h

--- v0.3/pasirinkimai.h
+++ v0.3/pasirinkimai.h
@@ -6,9 +6,15 @@
 #include "rusiavimas.h"
 
 void Skaiciavimai(std::vector<studentas>& studentai);
-void PirmasP(std::vector<studentas>& studentai);
-void AntrasP(std::vector<studentas>& studentai);
-void TreciasP(std::vector<studentas>& studentai, const std::vector<std::string>& pavardes,  const std::vector<std::string>& vardai);
-void KetvirtasP(std::vector<studentas>& studentai, const std::string& CVfd, const std::string& CVfr, double& suma2, double& suma3, double& suma4, double& suma5, double& suma6, int& spausd, bool& skaitytaIsFailo);
+void Spausdinimas(std::vector<studentas>& studentai, int& b);
+void VardasPavarde(std::vector<studentas>& studentai, studentas& s);
+int kinta();
+int kintrus(int& rus);
+int kintr(int& r);
+void RandPaz(std::vector<studentas>& studentai, studentas& s);
+void PirmasP(std::vector<studentas>& studentai, int& b);
+void AntrasP(std::vector<studentas>& studentai, int& b);
+void TreciasP(std::vector<studentas>& studentai, const std::vector<std::string>& pavardes,  const std::vector<std::string>& vardai, int& b);
+void KetvirtasP(std::vector<studentas>& studentai, const std::string& CVfd, const std::string& CVfr, double& suma2, double& suma3, double& suma4, double& suma5, double& suma6, int& spausd, bool& skaitytaIsFailo, int& b);
 
 #endif
