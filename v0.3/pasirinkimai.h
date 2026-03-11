ec8166dcbd46b7d4c462ccae758e41069f15df4c
GintareeJ <gjonaityte1@gmail.com>
2026-03-11 16:12:16 +0200

    Update pasirinkimai.h with new function declarations
    Added new functions for file reading and student sorting.

--- v0.3/pasirinkimai.h
+++ v0.3/pasirinkimai.h
@@ -11,12 +11,16 @@ void VardasPavarde(std::vector<studentas>& studentai, studentas& s);
 int kinta();
 int kintrus(int& rus);
 int kintr(int& r);
-void RandPaz(std::vector<studentas>& studentai, studentas& s);
+void RandPaz(std::vector<studentas>& studentai, studentas& s, int& kiek);
 void PirmasP(std::vector<studentas>& studentai, int& b);
 void AntrasP(std::vector<studentas>& studentai, int& b);
 void TreciasP(std::vector<studentas>& studentai, const std::vector<std::string>& pavardes,  const std::vector<std::string>& vardai, int& b);
 void KetvirtasP(std::vector<studentas>& studentai, const std::string& CVfd, const std::string& CVfr, double& suma2, double& suma3, double& suma4, double& suma5, double& suma6, int& spausd, bool& skaitytaIsFailo, int& b);
 void GeneruotiStudentuFaila(const std::string& failoPav, int studentuKiekis, int ndKiekis);
-void GeneruotiVisusFailus();
-
+void NuskaitytiIsFailo(std::vector<studentas>& studentai, const std::string& failoPav);
+void PadalintiStudentus(const std::vector<studentas>& studentai, std::vector<studentas>& vargsiukai, std::vector<studentas>& kietiakiai, int b);
+void RusiuotiStudentus(std::vector<studentas>& studentai, int b, int r, int rus);
+template <typename Konteineris>
+void SpausdintiIFaila(const std::string& failoPav, const Konteineris& studentai, int b);
+void PenktasP(std::vector<studentas>& studentai, std::vector<studentas>& vargsiukai, std::vector<studentas>& kietiakai);
 #endif
